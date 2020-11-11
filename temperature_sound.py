ed186d784bc19ddc7744f703ef7ec3ecc8037d28
sebakeien <56780805+sebakeien@users.noreply.github.com>
2020-11-11 15:35:08 -0300

    Update temperature_sound.py
    That code will be changing. I'm using a filter to understand about the frequency of water.
    The physics of the phenomenon is very simple: the viscosity of the water always change with temperature and the water sounds very different to the human.

--- temperature_sound.py
+++ temperature_sound.py
@@ -14,7 +14,7 @@ from numpy import argmax
 from IPython.display import Audio
 
 
-fs,data_signal=read("hola_hola.wav")
+fs,data_signal=read("First_temperature.wav")
 t=np.arange(len(data_signal))/fs
 plt.figure(figsize=(15,8))
 plt.plot(t,data_signal)
@@ -29,17 +29,17 @@ xff = fftfreq(len(data_signal), 1/fs)[:Nmuestras//2];
 
 plt.plot(xff, 2.0/Nmuestras * np.abs(yff[0:Nmuestras//2]))
 plt.grid()
-plt.title('Señal Original en el Dominio de la Frecuencia')
-plt.xlabel('Frecuencia (Hz)')
-plt.ylabel('Amplitud')
+plt.title('Original Signal')
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Amplitude')
 plt.show()
 print(fs)
 sos = signal.butter(5, 7000, fs=44100, output='sos')
 filtered = signal.sosfilt(sos, data_signal)
 plt.plot(t, filtered)
-plt.title('Señal con Filtro Paso Bajas de 7000Hz')
-plt.xlabel('Tiempo [seg]')
-plt.ylabel('Amplitud')
+plt.title('Signal with filter passlow 100Hz')
+plt.xlabel('Time [seg]')
+plt.ylabel('Amplitude')
 plt.show()
 
 Nmuestras = len(filtered)
@@ -48,10 +48,10 @@ xff = fftfreq(len(filtered), 1/fs)[:Nmuestras//2];
 
 plt.plot(xff, 2.0/Nmuestras * np.abs(yff[0:Nmuestras//2]))
 plt.grid()
-plt.title('Dominio de la Frecuencia')
-plt.xlabel('Frecuencia (Hz)')
-plt.ylabel('Amplitud')
+plt.title('Domain of frequency')
+plt.xlabel('Frequency [Hz]')
+plt.ylabel('Amplitude')
 plt.show()
 
-write("Voz_filtrada.wav", fs, filtered)
+write("temperature_filter.wav", fs, filtered)
 Audio(filtered, rate=fs)
